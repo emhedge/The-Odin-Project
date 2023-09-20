9a127896874af4daa0da3fa9b15396e8e022cb52
emhedge <122066271+emhedge@users.noreply.github.com>
2023-09-20 15:00:32 +0800

    Completed a few various minor exercises for js

---
+++ Foundations/javascript-basics/javascript.js
@@ -0,0 +1,19 @@
+// task 1 Working with variables
+// let name = "John";
+
+// let admin;
+
+// admin = name;
+
+// alert(admin)
+
+// task 2 Giving the right name
+// let ourPlanetName = "Earth";
+// let currentUserName = "John";
+
+// task 3 Uppercase const?
+// const BIRTHDAY = '08.12.1991';
+// const age = someCode(BIRTHDAY)
+// this is because BIRTHDAY's const will not change; the age will be evaluated in runtime
+
+
